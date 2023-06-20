598f015293c7229fa67fb80c8ada4a3179278787
David Ogwuche <victorochez@outlook.com>
2023-06-20 08:55:42 +0100

    Latest updates to Models and Views

---
+++ DignityWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,20 @@
+using DignityWebRazor_Temp.Data;
+using DignityWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DignityWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
